204574a66a69d026e5c584a5ce68303d31992329
janmelle <janmellepapa@gmail.com>
2025-09-21 18:28:15 +0800

    gallery page with static images

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    ctx = {"title": "Home", "features": ["Django", "Templates", "Static files"]}
+    return render(request, "home.html", ctx)
+
+def about(request):
+    return render(request, "about.html", {"title": "About"})
+
+def hello(request, name):
+    return render(request, "hello.html", {"name": name})
+
+def gallery(request):
+    # Assume images placed in pages/static/img/
+    images = ["img1.jpg", "img2.jpg", "img3.jpg"]
+    return render(request, "gallery.html", {"images": images})
